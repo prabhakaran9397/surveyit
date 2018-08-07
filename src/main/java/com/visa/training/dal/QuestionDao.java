a4a2feb3378a718513d43e22a573ab16e61a664c
Administrator <Administrator@172.31.7.144>
2018-08-07 11:28:23 +0100

    Added basic functionalities in QuestionDao and MySqlQuestionDao

--- src/main/java/com/visa/training/dal/QuestionDao.java
+++ src/main/java/com/visa/training/dal/QuestionDao.java
@@ -1,10 +1,22 @@
 package com.visa.training.dal;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.visa.training.domain.Question;
+
+
 
 @Repository
 @Transactional
 public interface QuestionDao {
 
+	Question create(Question question);
+	
+	void delete(int id);
+	
+	Question findById(int id);
+	
 }
