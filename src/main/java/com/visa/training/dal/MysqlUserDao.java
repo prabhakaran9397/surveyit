2a11d7b2b1b1683d480c5b0bb33d0cd47e4336d8
prabhakaran9397 <prabhakaran9397@gmail.com>
2018-08-07 11:10:23 +0100

    User basic functions declared

--- src/main/java/com/visa/training/dal/MysqlUserDao.java
+++ src/main/java/com/visa/training/dal/MysqlUserDao.java
@@ -1,5 +1,36 @@
 package com.visa.training.dal;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.visa.training.domain.User;
+
+@Repository
+@Transactional
 public class MysqlUserDao implements UserDao {
 
+	@Autowired
+	EntityManager em;
+	
+	@Override
+	public void delete(int id) {
+		User u = em.find(User.class, id);
+		em.remove(u);
+	}
+
+	@Override
+	public User create(User u) {
+		em.persist(u);
+		return u;
+	}
+
+	@Override
+	public User findById(int id) {
+		return em.find(User.class, id);
+	}
+	
+	
 }
--- src/main/java/com/visa/training/dal/UserDao.java
+++ src/main/java/com/visa/training/dal/UserDao.java
@@ -3,8 +3,16 @@ package com.visa.training.dal;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.visa.training.domain.User;
+
 @Repository
 @Transactional
 public interface UserDao {
 
+	User create(User u);
+	
+	void delete(int id);
+
+	User findById(int id);
+	
 }
