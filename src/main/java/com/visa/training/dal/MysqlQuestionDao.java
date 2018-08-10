f2eed1f2048aa300cfae7551f199fdaa0f35600e
prabhakaran9397 <prabhakaran9397@gmail.com>
2018-08-10 05:34:12 +0100

    fk one to many data is added

--- src/main/java/com/visa/training/dal/MysqlQuestionDao.java
+++ src/main/java/com/visa/training/dal/MysqlQuestionDao.java
@@ -1,5 +1,7 @@
 package com.visa.training.dal;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.visa.training.domain.Question;
+import com.visa.training.domain.Survey;
+import com.visa.training.domain.UserSurvey;
 
 @Repository
 @Transactional
@@ -31,6 +35,12 @@ public class MysqlQuestionDao implements QuestionDao {
 	@Override
 	public Question findById(int id) {
 		return em.find(Question.class, id);
+	}
+
+	@Override
+	public List<Question> findAllBySurvey(Survey s) {
+		return (List<Question>) em.createQuery("SELECT q FROM Question q WHERE q.survey.id = " + s.getId()).getResultList();
+
 	}
 	
 
